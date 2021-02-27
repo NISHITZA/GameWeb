1ded64ae964d3a431f2b0270ade66b6900db941b
Nishith A <nishithza@gmail.com>
2021-02-27 20:05:36 +0530

    cart update and remove functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Container, Typography,  Button, Grid } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 import useStyles from './styles'
 import CartItem from './CartItem/CartItem'
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles()
     
 
     const EmptyCart = () =>{
-       return( <Typography variant="subtitle1"> Your cart is empty </Typography>)
+       return( 
+            <Typography variant="subtitle1"> 
+                Your cart is empty 
+                <Link to="/" className={classes.link}>
+                    Add item to cart 
+                </Link>
+            </Typography>)
     }
 
     const FilledCart = () =>{
@@ -18,7 +25,7 @@ const Cart = ({ cart }) => {
                 {cart.line_items.map((item)=>(
                     <Grid item xs={12} sm={4} key={item.id}>
                         <div>
-                            <CartItem item={item} />
+                            <CartItem item={item} handleUpdateCartQty={handleUpdateCartQty} handleRemoveFromCart={handleRemoveFromCart}/>
                         </div>
                     </Grid>
                 ))}
@@ -26,7 +33,7 @@ const Cart = ({ cart }) => {
             <div className={classes.cardDetails}>
                 <Typography variant="h4">Subtotal:{ cart.subtotal.formatted_with_symbol }</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
                     <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
                 </div>
             </div>
